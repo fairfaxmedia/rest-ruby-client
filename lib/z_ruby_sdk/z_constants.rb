afda3379a49962a3224d7ff99c20173eeb25add9
Allan Andal <aandal@fairfaxmedia.com.au>
2015-02-18 14:57:56 +1100

    change config and log path

--- lib/z_ruby_sdk/z_constants.rb
+++ lib/z_ruby_sdk/z_constants.rb
@@ -5,8 +5,8 @@
 
 # For internal use only
 module Z_Constants
-  CONFIG_FILE = "z_config.yml"
-  LOG_DIR = "z_sdk_logs"
+  CONFIG_FILE = "config/z_config.yml"
+  LOG_DIR = "log/z_sdk_logs"
   LOG_FILENAME = "sdk.log"
   API_TRACE_FILENAME = "api_trace.log"
   CA_BUNDLE_PEM = "../config/z_ca_bundle.pem"
